4aa433b815325edf5e84cce5c84f53833ee0face
haruto8610 <haruto.fukushina0207@gmail.com>
2024-11-22 22:44:21 +0900

    logout

--- back/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ back/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -13,26 +13,29 @@ class AuthenticatedSessionController extends Controller
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): Response
+    public function store(LoginRequest $request)
     {
-        $request->authenticate();
-
-        $request->session()->regenerate();
-
-        return response()->noContent();
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials, $request->filled('remember'))){
+            return response()->json([
+                'message' => 'ログインに成功しました',
+                'user' => Auth::user(),
+            ]);
+        }
+        return response()->json(['message' => 'ログインに失敗しました'], 401);
     }
+
+
 
     /**
      * Destroy an authenticated session.
      */
-    public function destroy(Request $request): Response
+    public function destroy(Request $request)
     {
-        Auth::guard('web')->logout();
-
-        $request->session()->invalidate();
-
+        Auth::logout();
+        $request->session()->inalidate();
         $request->session()->regenerateToken();
 
-        return response()->noContent();
+        return response()->json(['message' => 'ログアウトしました']);
     }
 }
--- back/routes/api.php
+++ back/routes/api.php
@@ -6,19 +6,17 @@ use App\Http\Controllers\TodoController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
+Route::post('/register', [RegisteredUserController::class, 'store']);
+Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy']);
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/api', function() {
-    return response()->json([
-        "success" => true,
-        "message" => "hello laravel",
-        "hello" => "連携完了最高"
-    ]);
+
+Route::middleware('auth:sanctum')->get('/check-login', function () {
+    return response()->json(['message' => 'Logged in', 'user' => auth()->user()]);
 });
 
-Route::post('/register', [RegisteredUserController::class, 'store']);
-Route::post('/login', [AuthenticatedSessionController::class, 'store']);
 
 Route::apiResource('todos', TodoController::class)->only(['index', 'store', 'update', 'destroy']);
